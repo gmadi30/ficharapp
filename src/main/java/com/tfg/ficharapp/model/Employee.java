6e2ddc07bf059f8f44d93cd971e6b8974f26580c
gmadi30 <georgesmadi30@hotmail.com>
2022-08-13 13:39:15 +0200

    fix: Employee intity Id to test Git

--- src/main/java/com/tfg/ficharapp/model/Employee.java
+++ src/main/java/com/tfg/ficharapp/model/Employee.java
@@ -1,7 +1,13 @@
 package com.tfg.ficharapp.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Employee {
+
+    @Id
+    @Column
+    private String employeeId;
 }
